9e6d46bb85daf3d812c80fa9fefded6abee9d38e
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2021-01-18 19:00:29 -0300

    urls a https

--- dist/src/mapa.js
+++ dist/src/mapa.js
@@ -1,15 +1,15 @@
 // Mapa IGN nuevo (Arcgis Server)
 var argenmap = L.tileLayer('https://ide.ign.gob.ar/geoservicios/rest/services/Mapas_IGN/mapa_topografico/MapServer/tile/{z}/{y}/{x}', {
 	maxZoom: 18,
-	attribution: '<a href="http://www.ign.gob.ar">Instituto Geográfico Nacional</a>' +
-		' | &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'
+	attribution: '<a href="https://www.ign.gob.ar">Instituto Geográfico Nacional</a>' +
+		' | &copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a>'
 });
 
 var satelital = L.gridLayer.googleMutant({
 	type: 'satellite' // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
 });
 
-var referenciasOsm = L.tileLayer.wms('http://wms.ign.gob.ar/geoserver/wms', {
+var referenciasOsm = L.tileLayer.wms('https://wms.ign.gob.ar/geoserver/wms', {
 	layers: 'ideign:referencias_osm_mapa_contacto',
 	format: 'image/png',
 	transparent: true,
